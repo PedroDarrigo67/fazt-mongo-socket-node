8ec3bdbb08c8bae8291f1660905e2a99169f749b
PedroDarrigo67 <pedrodarrigo@gmail.com>
2023-03-08 18:47:39 -0300

    con html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import app from './app';
+import {Server as websocketServer} from 'socket.io';
+import http from 'http';
 
-app.listen(3000)
+//ambos servidores deben estar conectados, pero no directo
+//tengo servidor http y servidor de websicket
+const server = http.createServer(app)
+const httpServer  = server.listen(3000)
+const io = new websocketServer(httpServer)
+
+
 console.log('server en 3000')
